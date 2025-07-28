b6275100c934815f6bd20d62adfc60a6b479a700
msifatsadiq <msifatsadiq@gmail.com>
2025-07-28 19:46:26 +0600

    configuring and connecting MONGODB to server and create user schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth",authRoutes)
 
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB()
 })
